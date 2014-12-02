04e6b163b7859887202776890292988b34dc4712
Bog <berenger.ossete@live.fr>
2014-12-02 08:11:21 +0100

    ajout des fichiers .asm

---
+++ test_bne.asm
@@ -0,0 +1,10 @@
+addi $t0,$zero,5
+addi $t1,$zero,1
+
+tantQue:	
+sub $t1,$t1,$t0
+bne $t0,$zero,tantQue
+
+
+
+
---
+++ test_branchements.asm
@@ -0,0 +1,5 @@
+addi $t0, $zero, 42
+beq $t0, $t1 ,0
+bne $t0, $t1 ,4
+blez $t0 ,4
+bgtz $t0 ,4
