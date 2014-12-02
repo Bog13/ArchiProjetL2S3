80d67cb766917ca5b8fa3b2d390e16e4ffa08f10
Bog <berenger.ossete@live.fr>
2014-12-02 18:32:27 +0100

    Ajout bne beq blez bgtz + ajout fichier asm pour tester (test_branchements)

--- test_bne.asm
+++
@@ -1,8 +0,0 @@
-addi $t0,$zero,5
-addi $t1,$zero,1
-
-lbl:
-sub $t0,$t0,$t1
-bne $t0,$zero,lbl
-
-
--- test_branchements.asm
+++ test_branchements.asm
@@ -1,5 +1,30 @@
-addi $t0, $zero, 42
-beq $t0, $t1 ,0
-bne $t0, $t1 ,4
-blez $t0 ,4
-bgtz $t0 ,4
+#bne
+addi $t0,$zero,5
+addi $t1,$zero,1
+
+lbl1:
+sub $t0,$t0,$t1
+bne $t0,$zero,lbl1
+
+#beq
+addi $t0,$zero,5
+addi $t1,$zero,1
+
+lbl2:
+sub $t0,$t0,$t1
+beq $t0,$zero,lbl2
+	
+#blez
+addi $t0,$zero,-5
+
+lbl3:
+addi $t0,$t0,1
+blez $t0,lbl3
+	
+#bgtz
+addi $t0,$zero,5
+addi $t1,$zero,1
+
+lbl4:
+sub $t0,$t0,$t1
+bgtz $t0,lbl4
