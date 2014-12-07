f2347dd3e6a104605b88a05afaca7e6068a3ceb7
BoG <berenger.ossete@live.fr>
2014-12-07 15:46:01 +0100

    Gestion du shamt avec sll et srl implanté. Cependant ça ne marche toujours pas pour des raisons mystiques. modification des PLA pour prendre en compte sll et srl et ajout de rsshamt qui vaut 1 si on utilise le shamt et 0 si on utilise rs (utile pour pouvoir ensuite appeler le UAl avec l'op 8 et 9 (B<<A et B>>>A).

--- test_decalages.asm
+++ test_decalages.asm
@@ -1,3 +1,12 @@
-addi $t0, $0 , 0x3
-sll $t1 , $t0 , 1
-sra $t2 , $t1 , 2
+
+#DECALAGE sllv slrv
+#addi $t0, $zero, 1
+#
+#addi $t1, $0 , 0xf
+#sllv $t1 , $t1 , $t0
+#srlv $t1 , $t1 , $t0
+
+#DECALAGE sll srl
+addi $t1, $zero, 0xff
+srl $t1, $t1, 1
+sll $t1, $t1, 2
