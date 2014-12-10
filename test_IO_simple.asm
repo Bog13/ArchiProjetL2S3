10c0db484c57bbacc9b7879914af6d3e100d075f
Bog <berenger.ossete@live.fr>
2014-12-10 19:03:36 +0100

    le clavier prends les lettres et les stores en 0xffff000c

--- test_IO_simple.asm
+++ test_IO_simple.asm
@@ -1,8 +1,11 @@
 
-lui $t0, 0xFFFF
+
 addi $t1, $zero, 1
 boucle:
-	
+
+addi $t0, $zero,0
+lui $t0, 0xFFFF
+
 lw $t2,4($t0) # $t2 <-- Mem32[0xFFFF0000]
 sw $t2,0xC($t0) #Mem32[$t0 + 0xC = 0XFFFF000C] <-- $t2
 
